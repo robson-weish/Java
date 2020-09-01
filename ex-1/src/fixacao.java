28a5418df4f827304611812f9a195c78e3190fbe
Robson Gomes <60051587+robson-weish@users.noreply.github.com>
2020-08-31 23:17:49 -0300

    Completed

--- ex-1/src/fixacao.java
+++ ex-1/src/fixacao.java
@@ -1,9 +1,35 @@
+import java.util.Locale;
 
 public class fixacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String product1 = "Computer";
+		String product2 = "Office desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		 
+		
+		System.out.println("Products:");
+		System.out.printf(product1 + ", which price is $ %.2f %n", price1);
+		System.out.printf(product2 + ", which price is $ %.2f %n", price2);
+		
+		System.out.print("Record:");
+		System.out.printf(age + " years old, code " + code + " and gender: " + gender + " %n");
+		
+		System.out.printf("Measue with decimal places: %.10f %n", measure);
+		System.out.printf("Rouded (three decimal places): %.3f %n", measure);
+		
+		
+		// COMANDO QUE E NECESSARIO O IMPORT UTILIZADO ACIMA
+		Locale.setDefault(Locale.US);
+		System.out.printf("US decimal point: %.3f %n", measure);
 	}
 
 }
